e85bc69f1a8bc1f0d9a2b85d14e82426bf43f622
Joshna <jyothsnakasireddy04@gmail.com>
2025-02-23 09:51:16 +0000

    commit

--- variables/var.sh
+++ variables/var.sh
@@ -1,3 +1,46 @@
-#!/bin/basg
+#!/bin/bash
 
-echo "hello world"
+name="Luffy"
+echo $name    #output: Luffy
+
+echo "enter name: "   #enter a name/word
+read word             
+echo "The name is : $word "     
+
+#special variables 
+
+echo "script: $1 $2"
+
+VAR="DevOps engineer"
+echo ${#VAR}  # Output: 15
+NAME="Zoro"
+echo ${#NAME}   #counts the number of lettes in the given name
+
+
+name=gemini
+age=30
+
+echo "hello i am $name , i am $age years old "   #output: hello i am gemini , i am 30 years old
+
+
+#current substitution
+
+current_day=$(date +%A)
+
+echo "today day is: $current_day"    #output: today day is: Sunday
+
+
+#default_value
+name="gem"
+echo "hello, ${name:-siri}"       #output:hello, gem
+
+name="max"
+echo "hello, ${name:-guest}"      #output:hello, max 
+
+#read-only variable
+
+readonly anime=one_piece      #the variable cannot be overridden
+
+anime=jujutsu_kaisen
+
+echo "the name of the anime is $anime"    #output: the name of the anime is one_piece
